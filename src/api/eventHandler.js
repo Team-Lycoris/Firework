43e7eddd179cc907acc8f5af8a64077c1fd3df35
Chris Maruca <chrismaruca@ucla.edu>
2024-02-28 21:51:17 -0800

    Add handlers for more events

--- src/api/eventHandler.js
+++ src/api/eventHandler.js
@@ -1,15 +1,55 @@
 export default function eventHandler(io, socket) {
-  function handleMessage(data, callback) {
-    console.log(data);
-    callback({
-      status: "OK"
-    })
-  }
-
   function handleDisconnect(reason) {
     console.log('Client disconnected:', reason);
   }
+  
+  function handleMessage(data, callback) {
+    console.log("Message:", data);
+    callback({
+      status: 'OK'
+    });
+  }
 
-  socket.on('message', handleMessage);
+  function handleCreateChannel(data, callback) {
+    console.log("Create channel:", data);
+    callback({
+      status: 'OK'
+    });
+  }
+
+  function handleDeleteChannel(data, callback) {
+    console.log("Delete channel:", data);
+    callback({
+      status: 'OK'
+    });
+  }
+
+  function handleJoinChannel(data, callback) {
+    console.log("Join channel:", data);
+    callback({
+      status: 'OK'
+    });
+  }
+
+  function handleLeaveChannel(data, callback) {
+    console.log("Leave channel:", data);
+    callback({
+      status: 'OK'
+    });
+  }
+
+  function handleInviteToChannel(data, callback) {
+    console.log("Invite to channel:", data);
+    callback({
+      status: 'OK'
+    });
+  }
+  
   socket.on('disconnect', handleDisconnect);
+  socket.on('user:message', handleMessage);
+  socket.on('user:create-channel', handleCreateChannel);
+  socket.on('user:delete-channel', handleDeleteChannel);
+  socket.on('user:join-channel', handleJoinChannel);
+  socket.on('user:leave-channel', handleLeaveChannel);
+  socket.on('user:invite-to-channel', handleInviteToChannel);
 }
