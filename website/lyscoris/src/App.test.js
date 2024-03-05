033cf3734c6fa97fcedc79678946dd4c4012b186
Chris Maruca <chrismaruca@ucla.edu>
2024-03-04 22:01:05 -0800

    Move website files

--- website/lyscoris/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Homepage from './App';
-
-test('renders learn react link', () => {
-  render(<Homepage />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
