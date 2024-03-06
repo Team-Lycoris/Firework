1193979fd2a3b58b899ff0c4bc86465948f00b41
Chris Maruca <chrismaruca@ucla.edu>
2024-03-05 19:00:20 -0800

    Reimplement register and login functionality

--- server/src/controller/controller.js
+++ server/src/controller/controller.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import User from "../database/user.js";
+import UserHash from '../auth/userHash.js';
 
 const saltRounds = 10;
 
@@ -7,13 +7,13 @@ export async function register(req, res, next) {
     try {
         console.log(req);
 
-        const existingUser = await User.findOne({ where: { name: req.body.username }});
+        const existingUser = await UserHash.findOne({ where: { user: req.body.username }});
         if (existingUser !== null) {
             return res.json({status: false, msg: "User already exists"});
         }
-        const passwordHash = bcrypt.hash(req.body.password, saltRounds);
 
-        const user = await User.createUser(req.body.username, passwordHash);
+        const hash = await bcrypt.hash(req.body.password, saltRounds);
+        const user = await UserHash.create({user: req.body.username, hash: hash});
 
         return res.json({ status: true });
     } catch(ex) {
@@ -25,12 +25,12 @@ export async function login(req, res, next) {
     try {
         console.log(req);
 
-        const user = await User.findOne({ where: {name: req.body.username }});
+        const user = await UserHash.findOne({ where: {user: req.body.username }});
         if (!user) {
             return res.json({status: false, msg: "Incorrect username or password"});
         }
 
-        const matched = await bcrypt.compare(req.body.password, user.passwordHash);
+        const matched = await bcrypt.compare(req.body.password, user.hash);
         if (!matched) {
             return res.json({status: false, msg: "Incorrect username or password"});
         }
--- website/src/pages/login.js
+++ website/src/pages/login.js
@@ -1,24 +1,126 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../pages css/login.css';
+import axios from 'axios';
+
+const registerRoute = 'http://localhost:8080/api/auth/register';
+const loginRoute = 'http://localhost:8080/api/auth/login';
 
 const Authenticate = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  function handleValidate() {
+    setError('');
+    if (!checkUsername(username)) {
+      setError('Username must start with a letter and only consist of alphanumeric characters and ., _, -.');
+      return false;
+    }
+    if (!checkPassword(password)) {
+      setError('Password must only consist of alphanumeric and special characters.');
+      return false;
+    }
+    if (username.length == 0) {
+      setError('Please enter a username.');
+      return false;
+    }
+    if (username.length > 30) {
+      setError('Your username can be at most 30 characters long.');
+      return false;
+    }
+    if (password.length < 8) {
+      setError('Your password must be at least 8 characters long.');
+      return false;
+    }
+    if (password.length > 30) {
+      setError('Your password can be at most 30 characters long.');
+      return false;
+    }
+    return true;
+  }
+
+  async function handleRegister(event) {
+    event.preventDefault();
+    if (!handleValidate()) {
+      console.log(error);
+      return;
+    }
+    const res = await axios.post(registerRoute, {
+      username: username,
+      password: password
+    });
+    if (res.data.status) {
+      console.log('Successfully registered');
+    } else {
+      setError(res.data.msg);
+      console.log(res.data.msg);
+    }
+  }
+
+  async function handleLogin(event) {
+    event.preventDefault();
+    if (!handleValidate()) {
+      console.log(res.data.msg);
+      return;
+    }
+    const res = await axios.post(loginRoute, {
+      username: username,
+      password: password
+    });
+    console.log(res);
+    if (res.data.status) {
+      console.log('Successfully logged in');
+    } else {
+      setError(res.data.msg);
+      console.log(res.data.msg);
+    }
+  }
+
   return (
     <div className="login-container">
       <form className="login-form">
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" required />
+          <input 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="text" 
+            id="username" 
+            name="username" 
+            required 
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" required />
+          <input 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password" 
+            id="password" 
+            name="password" 
+            required />
         </div>
-        <button>Log In</button>
-        <buttonText>Sign Up</buttonText>
+        <button onClick={handleLogin}>Log In</button>
+        <buttonText onClick={handleRegister}>Sign Up</buttonText>
       </form>
     </div>
   );
 };
+
+function checkUsername(username) {
+  // Check if the username starts with a character and only consists
+  // of alphanumeric characters and some special characters
+  let regex = /^[a-zA-Z][a-zA-Z0-9._-]*$/
+  return regex.test(username);
+}
+
+function checkPassword(password) {
+  // Check if the password consists of only letters, numbers, and special characters
+  let regex1 = /^[a-zA-Z0-9\][~`!@#$%^&*()_+={}|\\;:"<>,.\/\?-]*$/
+  // Check if the password contains at least one special character
+  // let regex2 = /^.*[\][~`!@#$%^&*()_+={}|\\;:"<>,.\/\?-].*$/
+  return regex1.test(password);
+}
 
 export default Authenticate;
